7fe4101a46b983273f7b385c62a8eaf1ebd4dfb7
JonathanB <jonathanborgesandrade@gmail.com>
2024-08-14 23:07:16 -0300

    dio bank commit

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+    return (
+        <div>
+            Todos os direitos reservados.
+        </div>
+    )
+}
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import './Header.css'
+
+export const Header = () => {
+    return (
+        <div className='container'>
+            <h1 className='header'>
+                <a href="/"> Santo André Bank </a>
+            </h1>
+        </div>
+
+    )
+}
